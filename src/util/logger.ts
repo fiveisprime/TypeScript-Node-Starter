9ab6cc77b63af233d691bea935297411e40857a4
Matt Hernandez <maherna@microsoft.com>
2018-08-22 09:26:20 -0700

    Fix logging initialization.
    Added the missing dep and correctly init winston logger.

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,8 +1,7 @@
 import winston from "winston";
-import { Logger } from "winston";
 import { ENVIRONMENT } from "./secrets";
 
-const logger = new (Logger)({
+const logger = winston.createLogger({
     transports: [
         new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
         new (winston.transports.File)({ filename: "debug.log", level: "debug"})
